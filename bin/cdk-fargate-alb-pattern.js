54c55526b21160c675303c403a9b1df97e3acd7a
EC2 Default User <ec2-user@ip-172-31-28-117.us-east-2.compute.internal>
2020-11-10 01:17:24 +0000

    commit source code

--- bin/cdk-fargate-alb-pattern.js
+++ bin/cdk-fargate-alb-pattern.js
@@ -4,4 +4,7 @@ const cdk = require('@aws-cdk/core');
 const { CdkFargateAlbPatternStack } = require('../lib/cdk-fargate-alb-pattern-stack');
 
 const app = new cdk.App();
-new CdkFargateAlbPatternStack(app, 'CdkFargateAlbPatternStack');
+
+const _env  = { account: '457175632986', region: 'us-west-2' };
+
+new CdkFargateAlbPatternStack(app, 'CdkFargateAlbPatternStack', {env: _env});
